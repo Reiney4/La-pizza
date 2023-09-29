c9aed8aed73071765de4c94f93bf29f5d9eb2050
Valerie <valeriejeptoo4@gmail.com>
2023-09-29 12:56:09 +0300

    run app.py to make requests

--- server/app.py
+++ server/app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
